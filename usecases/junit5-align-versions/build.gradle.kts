1b7eac1597692dbef485cfbe8560ee70e9b182f5
Jendrik Johannes <jendrik@gradle.com>
2019-11-06 09:59:32 +0100

    Update JUnit sample

--- usecases/junit5-align-versions/build.gradle.kts
+++ usecases/junit5-align-versions/build.gradle.kts
@@ -3,12 +3,10 @@ plugins {
 }
 
 repositories {
-    maven { setUrl("snapshot-repo") } // <- repository with Gradle Module Metadata SNAPSHOT
-    maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0-SNAPSHOT")
-    implementation("org.junit.jupiter:junit-jupiter-engine:5.5.2") // should automatically align with core to 5.6.0-SNAPSHOT
+    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0-M1")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.5.2") // should automatically align with core to 5.6.0-M1
 }
